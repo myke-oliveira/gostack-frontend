f70177bd74c5024b20ce107b14eeeb89267b49bd
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-11-03 19:12:17 -0300

    Add babel

---
+++ public/script.js
@@ -0,0 +1,3 @@
+// Babel: Converter (transpilar) código do Reack para um código que o browser consiga entender.
+// Webpack: Para cada tipo de artigo (.js, .css, .png) eu preciso converter esse código de uma maneira diferente.
+// Loaders: babel-loader, css-loader, image-loader, file-loader.

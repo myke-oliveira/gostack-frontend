506876f701241555931107438ef8c1ecbf8e55b7
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-11-10 12:04:26 -0300

    use state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 function App() {
-  const projects = [
+  const [ projects, setProjects ] = useState([
     'Desenvolvimento de app',
     'Front-end web'
-  ];
+  ]);
 
   function handleAddProject() {
-    projects.push(`Novo projeto ${Date.now()}`);
+    setProjects([ ...projects, `Novo projeto ${Date.now()}` ]);
     console.log(projects);
   }
 
